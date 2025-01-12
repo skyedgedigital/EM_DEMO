81b0c993596a17383fc90219e1f98e16cb3369ae
paro-418 <vermapraween6543@gmail.com>
2025-01-12 22:22:33 +0530

    updated Master Data UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import NextTopLoader from 'nextjs-toploader';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'SHEKHAR ENTERPRISES',
+  title: 'Enterprise Management Demo',
   icons: '/assets/logo-bg.png',
 };
 
--- lib/actions/HR/workOrderHr/workOrderAction.ts
+++ lib/actions/HR/workOrderHr/workOrderAction.ts
@@ -1,19 +1,20 @@
 import { getEmpsInvolvedInWorkOrder } from "./empFromWorkOrder";
-import { createWorkOrderHr, deleteWorkOrderHr, fetchAllWorkOrderHr, fetchSingleWorkOrderHr, getTotalWorkOrder } from "./workOrderHr";
+import { createWorkOrderHr, deleteWorkOrderHr, fetchAllValidWorkOrderHr, fetchAllWorkOrderHr, fetchSingleWorkOrderHr, getTotalWorkOrder } from "./workOrderHr";
 
 const WorkOrderHrAction = {
-    CREATE:{
-        createWorkOrderHr:createWorkOrderHr
-    },
-    FETCH:{
-        fetchAllWorkOrderHr:fetchAllWorkOrderHr,
-        fetchSingleWorkOrderHr:fetchSingleWorkOrderHr,
-        fetchEmpsFromWorkOrder:getEmpsInvolvedInWorkOrder,
-        fetchTotalNumberOfWorkOrder:getTotalWorkOrder
-    },
-    DELETE:{
-        deleteWorkOrderHr:deleteWorkOrderHr
-    }
-}
+  CREATE: {
+    createWorkOrderHr: createWorkOrderHr,
+  },
+  FETCH: {
+    fetchAllWorkOrderHr: fetchAllWorkOrderHr,
+    fetchSingleWorkOrderHr: fetchSingleWorkOrderHr,
+    fetchEmpsFromWorkOrder: getEmpsInvolvedInWorkOrder,
+    fetchTotalNumberOfWorkOrder: getTotalWorkOrder,
+    fetchAllValidWorkOrderHr,
+  },
+  DELETE: {
+    deleteWorkOrderHr: deleteWorkOrderHr,
+  },
+};
 
 export default WorkOrderHrAction
