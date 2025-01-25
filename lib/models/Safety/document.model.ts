d605e570d2ee4f83483e0855bf1efa97eb2d16ce
Sanket Kumar <sanketcharanpahadi@gmail.com>
2025-01-25 13:36:08 +0530

    initial Document model

---
+++ lib/models/Safety/document.model.ts
@@ -0,0 +1,80 @@
+import mongoose, { Schema } from 'mongoose';
+
+interface Version {
+  versionNumber: number;
+  documentURL: string;
+  uploadedBy: mongoose.Types.ObjectId;
+  uploadDate: Date;
+}
+
+interface Document {
+  category: 'General' | 'SOP/JHA/HIRA';
+  documentType: string;
+  versions: Version[];
+  currentVersion: number;
+}
+
+const VersionSchema: mongoose.Schema<Version> = new mongoose.Schema({
+  versionNumber: {
+    type: Number,
+    required: true,
+  },
+  uploadedBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'EmployeeData',
+    required: true,
+  },
+  documentURL: {
+    type: String,
+    required: true,
+  },
+  uploadDate: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const DocumentSchema: mongoose.Schema<Document> = new mongoose.Schema(
+  {
+    category: {
+      type: String,
+      required: true,
+      enum: ['General', 'SOP/JHA/HIRA'],
+    },
+    documentType: {
+      type: String,
+      required: true,
+      enum: [
+        'Safety Manual',
+        'Policy & Principal',
+        'Organization Structure',
+        'Safety Plan',
+        'Objective & Target',
+        'PPE Replacement Policy',
+        'Tool Replacement Policy',
+        'Campaign Calendar',
+        'Emergency Preparedness Plan',
+        'Employee List',
+        'First-aider Certificate',
+        'Safety Professional Certificate',
+        'Top Management Certificate',
+        'Appointment Letter',
+        'Sponsorship Letter',
+        'SOP',
+        'JHA',
+        'HIRA',
+      ],
+    },
+    versions: [VersionSchema],
+    currentVersion: {
+      type: Number,
+      default: 1,
+    },
+  },
+  { timestamps: true }
+);
+
+export const DocumentModel = mongoose.model<Document>(
+  'Document',
+  DocumentSchema
+);
