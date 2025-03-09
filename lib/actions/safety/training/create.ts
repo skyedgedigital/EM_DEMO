f18b7e057555a2a5c6b429a9cc6422f2d32719a2
Sanket Kumar <sanketcharanpahadi@gmail.com>
2025-03-09 12:36:44 +0530

    created new server action and updated one server action

--- lib/actions/safety/training/create.ts
+++ lib/actions/safety/training/create.ts
@@ -151,6 +151,8 @@ export const createTrainingExamWithQuestions = async (
 };
 
 export const updateExam = async (
+  trainer: mongoose.Schema.Types.ObjectId,
+  trainingId: mongoose.Schema.Types.ObjectId,
   examId: mongoose.Schema.Types.ObjectId,
   updates: Partial<IExam>
 ): Promise<ApiResponse<IExam>> => {
@@ -158,8 +160,18 @@ export const updateExam = async (
     const dbConnection = await handleDBConnection();
     if (!dbConnection.success) return dbConnection;
 
-    if (!examId || Object.keys(updates).length === 0) {
+    if (!examId || !trainingId || Object.keys(updates).length === 0) {
       throw new Error('Invalid input: examId and updates are required');
+    }
+
+    // only the trainer can edit the training
+    const trainingExist = await TrainingModel.findOne({
+      _id: trainingId,
+      trainer,
+    });
+
+    if (!trainingExist) {
+      throw new Error('Training does not exist');
     }
 
     const updatedExam = await ExamModel.findByIdAndUpdate(examId, updates, {
@@ -177,6 +189,59 @@ export const updateExam = async (
         status: 200,
         message: 'Exam updated successfully!',
         data: updatedExam,
+        error: null,
+      })
+    );
+  } catch (error) {
+    return JSON.parse(
+      JSON.stringify({
+        success: false,
+        status: 400,
+        message: error.message || 'Something went wrong!',
+        data: null,
+        error: error,
+      })
+    );
+  }
+};
+
+export const updateTraining = async (params: {
+  trainingId: mongoose.Schema.Types.ObjectId;
+  trainer: mongoose.Schema.Types.ObjectId;
+  updates: Partial<ITraining>;
+}): Promise<ApiResponse<ITraining>> => {
+  try {
+    const dbConnection = await handleDBConnection();
+    if (!dbConnection.success) return dbConnection;
+
+    const { trainingId, trainer, updates } = params;
+
+    if (!trainingId || !trainer) {
+      throw new Error('Please provide valid trainingId and trainer');
+    }
+
+    const updated_training = await TrainingModel.findOneAndUpdate(
+      {
+        _id: trainingId,
+        trainer,
+      },
+      updates,
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
+
+    if (!updated_training) {
+      throw new Error("Couldn't update training, something went wrong");
+    }
+
+    return JSON.parse(
+      JSON.stringify({
+        success: true,
+        status: 200,
+        message: 'Training Successfully updated',
+        data: updated_training,
         error: null,
       })
     );
