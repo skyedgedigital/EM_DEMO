d0764929a4725aae166134e97df0432b12cb6c34
paro-418 <vermapraween6543@gmail.com>
2025-03-31 12:40:09 +0530

    removed bug in employee data interface

--- interfaces/HR/EmployeeData.interface.ts
+++ interfaces/HR/EmployeeData.interface.ts
@@ -1,43 +1,46 @@
-import mongoose from "mongoose";
-
+import mongoose from 'mongoose';
 
 export interface IEmployeeData {
-
-    // employeeDetails:{
-        code:string;
-        workManNo:string;
-        name:string;
-        department:mongoose.Schema.Types.ObjectId;
-        site:mongoose.Schema.Types.ObjectId;
-        designation:mongoose.Schema.Types.ObjectId;
-        bank:mongoose.Schema.Types.ObjectId;
-        accountNumber:string;
-        pfApplicable:boolean;
-        pfNo:string;
-        UAN:string;
-        ESICApplicable:boolean;
-        ESICNo:string;
-        ESILocation:mongoose.Schema.Types.ObjectId;
-        adhaarNumber:string;
-    // },
-    // personalInformation:{
-        sex:"Male"|"Female"|"TransGender";
-        martialStatus:"married"|"unmarried"|"choose to not disclose";
-        dob:string;
-        attendanceAllowance:boolean;
-        fathersName:string;
-        address:string;
-        landlineNumber:string;
-        mobileNumber:string;
-        workingStatus:boolean;
-        appointmentDate:string;
-        resignDate:string;
-    // },
-    // otherDetails:{
-        safetyPassNumber:string;
-        SpValidity:string;
-        policeVerificationValidityDate:string;
-        gatePassNumber:string;
-        gatePassValidTill:string;
-    // }
+  // employeeDetails:{
+  code: string;
+  workManNo: string;
+  name: string;
+  department: mongoose.Schema.Types.ObjectId;
+  site: mongoose.Schema.Types.ObjectId;
+  designation: mongoose.Schema.Types.ObjectId;
+  bank: mongoose.Schema.Types.ObjectId;
+  accountNumber: string;
+  pfApplicable: boolean;
+  pfNo: string;
+  UAN: string;
+  ESICApplicable: boolean;
+  ESICNo: string;
+  ESILocation: mongoose.Schema.Types.ObjectId;
+  adhaarNumber: string;
+  // },
+  workOrderHr: {
+    period: string;
+    workOrderHr: mongoose.Types.ObjectId;
+    workOrderAtten: number;
+  }[];
+  // personalInformation:{
+  sex: 'Male' | 'Female' | 'TransGender';
+  martialStatus: 'married' | 'unmarried' | 'choose to not disclose';
+  dob: string;
+  attendanceAllowance: boolean;
+  fathersName: string;
+  address: string;
+  landlineNumber: string;
+  mobileNumber: string;
+  workingStatus: boolean;
+  appointmentDate: string;
+  resignDate: string;
+  // },
+  // otherDetails:{
+  safetyPassNumber: string;
+  SpValidity: string;
+  policeVerificationValidityDate: string;
+  gatePassNumber: string;
+  gatePassValidTill: string;
+  // }
 }
