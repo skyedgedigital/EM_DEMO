f1687234ac66f4303d1dc5d5a8892b8347e7314d
Sanket Kumar <sanketcharanpahadi@gmail.com>
2025-04-02 14:52:18 +0530

    get next revNo server action

--- lib/actions/ppe/ppeAction.ts
+++ lib/actions/ppe/ppeAction.ts
@@ -1,40 +1,59 @@
-import { deleteAuditById, genAudit, getAuditAll } from "./audit";
-import { deleteCheckListById, genCheckList, getCheckListAll } from "./checkList";
-import { createPpe, deletePpe, fetchPpeAll, fetchPpeById, updatePpe } from "./ppe";
-import { createPpeIssue, deletePpeIssue, fetchPpeIssue } from "./ppeIssue";
-import { deletePpeIssueAndReplacement, fetchPpeIssueAndReplacement, genPpeIssueAndReplacement } from "./ppeIssueAndReplacement";
-import { createPpePurchase, deletePpePurchase, fetchPpePurchases } from "./ppePurchase";
+import { deleteAuditById, genAudit, getAuditAll } from './audit';
+import {
+  deleteCheckListById,
+  genCheckList,
+  getCheckListAll,
+} from './checkList';
+import {
+  createPpe,
+  deletePpe,
+  fetchPpeAll,
+  fetchPpeById,
+  updatePpe,
+} from './ppe';
+import { createPpeIssue, deletePpeIssue, fetchPpeIssue } from './ppeIssue';
+import {
+  deletePpeIssueAndReplacement,
+  fetchNextRevNoByDocumentNumber,
+  fetchPpeIssueAndReplacement,
+  genPpeIssueAndReplacement,
+} from './ppeIssueAndReplacement';
+import {
+  createPpePurchase,
+  deletePpePurchase,
+  fetchPpePurchases,
+} from './ppePurchase';
 
 const PpeAction = {
-    CREATE:{
-        createPpe:createPpe,
-        createPpeIssue:createPpeIssue,
-        createPpePurchase:createPpePurchase,
-        createCheckList:genCheckList,
-        createAudit:genAudit,
-        createPpeIssueAndReplacement:genPpeIssueAndReplacement
-    },
-    DELETE:{
-        deletePpe:deletePpe,
-        deletePpeIssue:deletePpeIssue,
-        deleltePpePurchase:deletePpePurchase,
-        deleteAudit:deleteAuditById,
-        deleteCheckListById:deleteCheckListById,
-        deletePpeIssueAndReplacement:deletePpeIssueAndReplacement
-    },
-    FETCH:{
-        fetchPpeAll:fetchPpeAll,
-        fetchPpeById:fetchPpeById,
-        fetchPpeIssuesAll:fetchPpeIssue,
-        fetchPpePurchases:fetchPpePurchases,
-        fetchAllCheckList:getCheckListAll,
-        fetchAllAudit:getAuditAll,
-        fetchPpeIssueAndReplacement:fetchPpeIssueAndReplacement
-    },
-    UPDATE:{
-        updatePpe:updatePpe,
-    },
-    
-}
+  CREATE: {
+    createPpe: createPpe,
+    createPpeIssue: createPpeIssue,
+    createPpePurchase: createPpePurchase,
+    createCheckList: genCheckList,
+    createAudit: genAudit,
+    createPpeIssueAndReplacement: genPpeIssueAndReplacement,
+  },
+  DELETE: {
+    deletePpe: deletePpe,
+    deletePpeIssue: deletePpeIssue,
+    deleltePpePurchase: deletePpePurchase,
+    deleteAudit: deleteAuditById,
+    deleteCheckListById: deleteCheckListById,
+    deletePpeIssueAndReplacement: deletePpeIssueAndReplacement,
+  },
+  FETCH: {
+    fetchPpeAll: fetchPpeAll,
+    fetchPpeById: fetchPpeById,
+    fetchPpeIssuesAll: fetchPpeIssue,
+    fetchPpePurchases: fetchPpePurchases,
+    fetchAllCheckList: getCheckListAll,
+    fetchAllAudit: getAuditAll,
+    fetchPpeIssueAndReplacement: fetchPpeIssueAndReplacement,
+    fetchNextRevNoByDocumentNumber,
+  },
+  UPDATE: {
+    updatePpe: updatePpe,
+  },
+};
 
-export default PpeAction
+export default PpeAction;
--- lib/actions/ppe/ppeIssueAndReplacement.ts
+++ lib/actions/ppe/ppeIssueAndReplacement.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { ApiResponse } from '@/interfaces/APIresponses.interface';
 import handleDBConnection from '@/lib/database';
 import PpeIssueAndReplacement from '@/lib/models/safetyPanel/ppe/ppeIssueAndReplacement.model';
 
@@ -68,8 +69,41 @@ const fetchPpeIssueAndReplacement = async () => {
   }
 };
 
+const fetchNextRevNoByDocumentNumber = async (
+  docNo: string
+): Promise<ApiResponse<{ nextRevNo: number }>> => {
+  try {
+    const dbConnection = await handleDBConnection();
+    if (!dbConnection.success) return dbConnection;
+    if (!docNo || docNo.trim().length === 0) {
+      throw new Error('invalid docNo');
+    }
+
+    const currentRevNo = await PpeIssueAndReplacement.countDocuments({
+      id: docNo,
+    });
+
+    return {
+      success: true,
+      status: 200,
+      message: 'Successfull',
+      data: { nextRevNo: currentRevNo + 1 },
+      error: null,
+    };
+  } catch (error) {
+    return {
+      success: false,
+      status: 500,
+      message: error instanceof Error ? error.message : 'An Error Occurred',
+      data: null,
+      error: error,
+    };
+  }
+};
+
 export {
   genPpeIssueAndReplacement,
   deletePpeIssueAndReplacement,
   fetchPpeIssueAndReplacement,
+  fetchNextRevNoByDocumentNumber,
 };
